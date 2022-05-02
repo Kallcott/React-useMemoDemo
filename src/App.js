bcac6c02a29a596709b8db96e7cb0238ecde689c
Kallcott <KeenanAllcott@live.ca>
2022-05-01 22:09:32 -0300

    finished tutorial

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useMemo, useEffect} from "react";
 
-function App() {
+// Case 1: cache slow function for speed.
+// use Memo is for use memoization. caches the value of a function to use again and again. 
+// we don't memoise everything because of the memory overheaad. only use when needed
+
+// Case 2: referential equality
+// when you compare two different variables in JS, we compare their reference.
+// we can cache objects so we can compare them. 
+
+export default function App() {
+  const [number, setNumber] = useState(0)
+  const [dark, setDark] = useState(false)
+
+  // Case 1
+  // effect1: change Theme will be very fast
+  // effect2: changing number bc cache update due to number dependancy
+  const doubleNumber = useMemo(() => {
+    return slowFunction(number)
+  }, [number])
+
+ //Case 2
+ // themestyles 1 and 2 are not equivalent because they reference different objects
+ //  const themeStyles2 = {
+ //    backgroundColor: dark ? 'black' : 'white',
+ //    color: dark ? 'white' : 'black'
+ // }
+  const themeStyles = useMemo(() => {
+    return {
+      backgroundColor: dark ? 'black' : 'white',
+      color: dark ? 'white' : 'black'  
+    }
+ }, [dark]) // here our object relies on dark, so update cache when dark changes 
+ 
+ // Case 1
+ // problem: when we call our number we are theme changed is also being run
+ // this is because themeStyles reliance can't equate two objects, so we wrap in a useMemo
+ useEffect(() => {
+   console.log('Theme Changhed')
+ }, [themeStyles])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+   <>
+  <input type="number" value={number} onChange={e => setNumber(parseInt(e.target.value))} />
+    <button onClick={() =>  setDark(prevDark => !prevDark)}i>Change Theme</button>
+  <div style={themeStyles}>{doubleNumber}</div>
+   </> 
+  ) 
 }
 
-export default App;
+//Makes app very slow, both set number and change theme
+function slowFunction(num){
+  console.log('Calling Slow Function');
+  for (let i = 0; i <= 300000000; i++) {}
+  return num + 2
+}
